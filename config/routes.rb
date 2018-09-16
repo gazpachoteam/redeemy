d5d7ce342b1691fb78a57ce64e2ae662ec8c0108
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-09-16 11:26:21 -0500

    changes on crear proveedores view

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
   get 'dashboard', to: "dashboard/dashboard#index", as: "bank_dashboard"
   namespace :dashboard do
     resources :providers do
+
     end
+    
   end
 
   get 'provider_admin/:provider_id', to: "provider_admin/dashboard#index", as: "dashboard_for_provider"
