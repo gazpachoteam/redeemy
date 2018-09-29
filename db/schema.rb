6f3154f41abaa5219dc733110a26cd73067a0c64
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-09-29 15:29:54 -0500

    In this branch we added some cancancan gem configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_012254) do
+ActiveRecord::Schema.define(version: 2018_09_27_210925) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer "customer_id"
@@ -77,6 +77,18 @@ ActiveRecord::Schema.define(version: 2018_09_20_012254) do
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
